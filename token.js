8c238dceca75e3d789fa74d91b48f056ac4b903f
marc-andre-colosse <marc-andre@colosse.ca>
2026-06-15 12:20:27 -0400

    remove_spotify

--- token.js
+++ token.js
@@ -2,7 +2,7 @@ const getSpotifyToken = async () => {
     // --- TES INFORMATIONS À REMPLIR ---
     const clientId = '95e5ac1979184ad99eb4841b91f6f801';
     const clientSecret = '071a2924e1914a86a6e5ca05a7a9ce6d';
-    const code = 'AQCl2fWHiJEebdOqnJvq_RVEWeuJO0hKGeAgPOKkrLNuUdk8_07h1_GZ9PLLAE0IAPkfSTBmUzIbBL5ieGlCpDY42pNW3ZveFLfNLjpZjp-EJRAvirI8YpbgGrJZDjrPPVQHlm7SSuOzAVEa77X51OysKPYSZatR3p3nWPFDc9aZY8_m3nNfvLp1a-_Tjdkm7DEQt0p5fxDwJWLkA02nioSIH5Y8wecFo2IOtc4'; 
+    const code = 'AQAK9kPWcgu8Cr9cOjEVfHrSck22yWPpU4YA8Ae_tHvm7T479krsl3KdzgLt53Rd1lKlPbsh6kIU8A3slkq8eN0m5aE0sQizA5jEYeOhqT1u42iTnWx4rqqNLoIq058pTZe0Lb1sLZpFzcTCLcMUdU85BJ8CyB_2f9HaWsCkQzq6qbb8WKFza6GKtToYgCWxdzMiyUGqXZrTS-28WHZd0jqRpKiP3xSBB58Ubtw'; 
 
     try {
         const response = await fetch('https://accounts.spotify.com/api/token', {
