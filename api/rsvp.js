6d417449839052d135ea760304404f54be72e852
marc-andre-colosse <marc-andre@colosse.ca>
2026-06-15 11:47:36 -0400

    Fix_again

--- api/rsvp.js
+++ api/rsvp.js
@@ -20,7 +20,7 @@ async function getSpotifyAccessToken() {
     });
     const data = await response.json();
     
-    // LA NOUVELLE LUMIÈRE : Si Spotify refuse le Refresh Token, on l'affiche !
+    // LA NOUVELLE LUMIÈRE : Si Spotify refuse le Refresh Token, on l'affiche!
     if (!response.ok) {
         console.error('❌ Erreur de Jeton (Token) Spotify :', data);
         return null;
